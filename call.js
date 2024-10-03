864bade4ab0c1f3ee1899be2bbe4310478d96a5c
Kajal-Srivastav <kajal-5>
2024-10-03 09:49:27 +0530

    .call() is used to set the context (this) for the function.

---
+++ call.js
@@ -0,0 +1,31 @@
+function fun1(name){
+    this.name=name;
+    console.log('name');
+}
+
+function fun2(name,classno){
+
+    fun1.call(this.name);
+    this.classno=classno;
+    console.log(name,classno);
+
+}
+
+const res = new fun2('kajal',9);
+// console.log(res);
+
+
+function greet(){
+    console.log(`hello ${this.name} ${this.subject}`);
+}
+
+const obj={name:'kajal'};
+
+function f1(name,subject){
+    this.name=name;
+    this.subject=subject;
+}
+
+greet.call(obj);
+let result = new f1('kajal','Math'); 
+greet.call(result);
